9e207053121b37fced6784634b4cc157314f1928
David Carvalho <david.snt.carvalho@gmail.com>
2017-08-23 16:56:45 -0300

    AlertController - Lidando com o inesperado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, LoadingController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
@@ -15,7 +15,8 @@ export class HomePage {
   constructor(
     public navCtrl: NavController,
     private _http: Http,
-    private _loadingCtrl: LoadingController
+    private _loadingCtrl: LoadingController,
+    private _alertCtrl: AlertController
   ) {
 
     let loader = this._loadingCtrl.create({
@@ -29,9 +30,20 @@ export class HomePage {
       .map(res => res.json())
       .toPromise()
       .then(carros => {
+        this.carros = carros;
         loader.dismiss();
-        this.carros = carros
-      });
+      }, err => {
+        console.log(err);
+        loader.dismiss();
+        this._alertCtrl
+          .create({
+            title: 'Falha na conexão',
+            buttons: [{
+              text: 'Estou ciente!'
+            }],
+            subTitle: 'Não foi possível obter a lista de carros, tente mais tarde'
+          }).present();
+      })
   }
 
 }
