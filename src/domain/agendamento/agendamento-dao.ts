1c026ea03891bcc9b836253b02e9fc95b2759fe8
David Carvalho <david.snt.carvalho@gmail.com>
2017-09-05 22:27:31 -0300

    Listando agendamentos

--- src/domain/agendamento/agendamento-dao.ts
+++ src/domain/agendamento/agendamento-dao.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 
 import { Agendamento } from "./agendamento";
+import { Carro } from "../carro/carro";
 
 @Injectable()
 export class AgendamentoDao {
@@ -27,4 +28,21 @@ export class AgendamentoDao {
               return dado ? true : false;
             });
   }
+
+  listaTodos() {
+    let agendamentos: Agendamento[] = [];
+
+    return this._storage.forEach(dado => {
+        let carro = new Carro(dado.carro.nome, dado.carro.preco);
+        let agendamento = new Agendamento(
+                carro, dado.valor,
+                dado.nome, dado.endereco,
+                dado.email,
+                dado.data,
+                dado.confirmado);
+
+        agendamentos.push(agendamento);
+    })
+    .then(() => agendamentos );
+}
 }
--- src/pages/agendamentos/agendamentos.ts
+++ src/pages/agendamentos/agendamentos.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Agendamento } from "../../domain/agendamento/agendamento";
+import { AgendamentoDao } from "../../domain/agendamento/agendamento-dao";
+
 @IonicPage()
 @Component({
   templateUrl: 'agendamentos.html',
 })
 export class AgendamentosPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  // esta propriedade alimentará o formulário
+  public agendamentos: Agendamento[];
+
+  constructor(
+      public navCtrl: NavController, 
+      public navParams: NavParams,
+      private _agendamentoDao: AgendamentoDao) {
+
+    this._agendamentoDao
+      .listaTodos()
+      .then(agendamentos => this.agendamentos = agendamentos);
+
   }
 
 }
